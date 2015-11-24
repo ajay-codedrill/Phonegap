555928982d7649cf6c25e2a603e6e07177cba85c
ajay-codedrill <ajay.codedrill@gmail.com>
2015-11-24 12:47:29 +0530

    24 Nov 2015

--- Healthpass/www/js/index.js
+++ Healthpass/www/js/index.js
@@ -1,14 +1,23 @@
 $(function(){
 $( "#pageone" ).on( "swipeleft", proceedtoapp );
 $( "#pageone" ).on( "swiperight", proceedtoapp );
- 
-  
 function proceedtoapp( event ){
 window.location.href="app.html";
 // $.mobile.changePage("app.html" , {   reloadPage : true, transition: "slide"});
 } 
-
-
 $( "body>[data-role='panel']" ).panel();
 });
 
+$( document ).on( "pageinit", function() {
+    $( document ).on( "swiperight", function( e ) {
+        if ( $.mobile.activePage.jqmData( "panel" ) !== "open" ) {
+			if ($.mobile.activePage.is("#pagetwo"))
+			{
+			return;
+			}
+             if ( e.type === "swiperight" ) {
+                $( "#leftpanel1" ).panel( "open" );
+            }
+        }
+    });	
+});
